0e1918d0d2303de72621b864cea5794d943c6bc6
NavyaBabyJ <142658617+NavyaBabyJ@users.noreply.github.com>
2023-09-11 14:33:48 +0530

    Add files via upload

---
+++ bank.java
@@ -0,0 +1,11 @@
+package com.nissan.service;
+
+public interface bank {
+	public void addCustomer();
+	public void generateNineDigitNumber();
+	public void updateCustomerDetails();
+	public void deleteCustomer();
+	public void displayAllCustomers();
+	public void displayCustomerDetails();
+
+}
